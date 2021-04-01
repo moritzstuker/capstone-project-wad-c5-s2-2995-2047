fc94e133f59c1142c09045e6c86b6d7dd723700a
Moritz Stuker <968496+moritzstuker@users.noreply.github.com>
2021-04-01 15:56:44 +0200

    Adds some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+admin  = User.create!(username: 'admin@test.dev',  name: 'Admin',  password: 'password', role: 'admin')
+john   = User.create!(username: 'john@test.dev',   name: 'John',   password: 'password', role: 'partner')
+paul   = User.create!(username: 'paul@test.dev',   name: 'Paul',   password: 'password', role: 'associate')
+george = User.create!(username: 'george@test.dev', name: 'George', password: 'password', role: 'client')
+ringo  = User.create!(username: 'ringo@test.dev',  name: 'Ringo',  password: 'password', role: 'client')
