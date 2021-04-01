620125291476066f7ebd03842e3f7bb70cbf97b9
Moritz Stuker <968496+moritzstuker@users.noreply.github.com>
2021-04-01 17:45:09 +0200

    Adds session routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  resources :users
-  get 'home/index'
-
-  root "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  root "home#index"
+
+  # Users routes
+  resources :users, only: [:new, :create, :edit, :update, :show, :destroy]
+
+  # Session routes
+  get  '/login',  to: 'sessions#login'
+  post '/login',  to: 'sessions#create'
+  get  '/logout', to: 'sessions#destroy'
+  post '/logout', to: 'sessions#destroy'
+  end
