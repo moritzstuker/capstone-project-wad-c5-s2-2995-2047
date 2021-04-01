fe7eb92f7445de0af18d8a7398266129366c768a
Moritz Stuker <968496+moritzstuker@users.noreply.github.com>
2021-04-01 17:45:22 +0200

    Adds session

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ActionController::Base
+  def create
+    @user = User.find_by(username: params[:username])
+
+    if !!@user && @user.authenticate(params[:username])
+      #set session and redirect on success
+      session[:user_id] = @user.id
+      redirect_to user_path
+    else
+      # error message on fail
+      message = "Whoops"
+      redirect_to login_path, notice: message
+    end
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
 
   # GET /users/1 or /users/1.json
   def show
+    @user = User.find(params[:id])
   end
 
   # GET /users/new
@@ -25,6 +26,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
+        session[:user_id] = @user.id
         format.html { redirect_to @user, notice: "User was successfully created." }
         format.json { render :show, status: :created, location: @user }
       else
