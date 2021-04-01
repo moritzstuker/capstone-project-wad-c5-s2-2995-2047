54561d88ddb55e7cf970d69423866a49665cd3fd
Moritz Stuker <968496+moritzstuker@users.noreply.github.com>
2021-04-01 15:41:25 +0200

    Switches to js engine

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,7 @@ module CapstoneProposalWadC5S129952047
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.generators.javascript_engine = :js
+
   end
 end
