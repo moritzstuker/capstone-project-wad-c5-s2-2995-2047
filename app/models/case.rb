7a4d3727f562bbd255f9e5327e906c2ec7d5c817
Moritz Stuker <968496+moritzstuker@users.noreply.github.com>
2021-04-02 07:26:27 +0200

    Adds cases

---
+++ app/models/case.rb
@@ -0,0 +1,7 @@
+class Case < ApplicationRecord
+  has_many :timesheets
+  has_many :users
+
+  validates_presence_of :name, :description
+  validates_uniqueness_of :name
+end
---
+++ app/views/cases/_case.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! case, :id, :name, :description, :created_at, :updated_at
+json.url case_url(case, format: :json)
