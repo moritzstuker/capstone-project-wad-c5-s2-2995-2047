873c69ee3ae3dd1701867f7ee9dac00f1bdd07ce
Moritz Stuker <968496+moritzstuker@users.noreply.github.com>
2021-04-05 07:47:39 +0200

    Adds user methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   add_flash_types :info, :success, :danger
+
+  def current_user
+    if @current_user.present?
+      return @current_user
+    end
+    @current_user = User.find(session[:user_id])
+  end
+
+  def logged_in?
+    session[:user_id].present?
+  end
+
+  def authenticated?
+    redirect_to login_path unless logged_in?
+  end
 end
