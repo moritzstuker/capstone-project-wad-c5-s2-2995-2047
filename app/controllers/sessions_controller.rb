36d8f5375cc34fd04163fce42d46a098262ff359
Moritz Stuker <968496+moritzstuker@users.noreply.github.com>
2021-04-06 08:41:31 +0200

    Minor tweaks

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       if @user.present? && @user.authenticate(params[:password])
         session[:user_id] = @user.id
 
-        format.html { redirect_to @user }
+        format.html { redirect_to dashboard_path }
       else
         format.html { redirect_to new_session_url, alert: "Not good" }
       end
@@ -20,7 +20,7 @@ class SessionsController < ApplicationController
     session.delete(:user_id)
 
     respond_to do |format|
-      format.html { redirect_to root_url, notice: "Good" }
+      format.html { redirect_to root_url, info: "Session destroyed." }
     end
   end
 end
