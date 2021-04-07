9807682c9427b4f455fcea7f4d90cb02aa372534
Moritz Stuker <968496+moritzstuker@users.noreply.github.com>
2021-04-07 16:22:55 +0200

    Adds active page class

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
   def current_user
     @current_user ||= User.find_by_id(session[:user_id]) if logged_in?
   end
+
+  def active_page?(path)
+    " is-active" if request.path == path
+  end
 end
