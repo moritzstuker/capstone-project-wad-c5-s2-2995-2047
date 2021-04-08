8a55a93a6f800f5b5c6380c4e92557273d282225
Moritz Stuker <968496+moritzstuker@users.noreply.github.com>
2021-04-08 07:31:38 +0200

    Added default image tag

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,22 @@
 class User < ApplicationRecord
+  has_and_belongs_to_many :projects
+
   has_secure_password
 
   validates :username, uniqueness: true
+  validates :role,  inclusion: { in: %w(client associate partner admin) }
 
-  has_and_belongs_to_many :projects
+  after_initialize  :defaults
+  before_validation :downcase_email
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  def defaults
+    self.role ||= 'client'
+    self.avatar ||= 'jean-beller-8ndbKLm3wCM-unsplash.jpg'
+  end
 end
