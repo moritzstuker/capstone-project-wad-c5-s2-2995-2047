7b97ffaab4d33e6c851c1d587fcecd50a45b4872
Moritz Stuker <968496+moritzstuker@users.noreply.github.com>
2021-04-19 11:13:45 +0200

    Adds dashboard

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,9 @@
+class DashboardController < ApplicationController
+  before_action :logged_in?
+  
+  def show
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
   root to: 'home#index'
 
+  resources :contacts
+  resources :users
+
   get    '/signup', to: 'users#new'
+
   get    '/login',  to: 'sessions#new'
   post   '/login',  to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
+  get    '/dashboard', to: 'dashboard#show'
+  get    '/dashboard/edit'
+
+  get    '/account', to: 'account#show'
+  get    '/account/edit'
 end
