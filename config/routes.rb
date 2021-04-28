5d84df141c55571ca7339f47616777f8be849676
Moritz Stuker <968496+moritzstuker@users.noreply.github.com>
2021-04-28 23:47:55 +0200

    Adds models to router

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   root to: 'home#index'
 
-  resources :contact
-  resources :project
-  resources :user
+  resources :activities
+  resources :contacts
+  resources :deadlines
+  resources :projects
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
