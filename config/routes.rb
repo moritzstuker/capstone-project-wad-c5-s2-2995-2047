ddafc562a1bf9be7e0e4c49e3a1c21916e6886ee
Moritz Stuker <968496+moritzstuker@users.noreply.github.com>
2021-05-05 07:38:04 +0200

    Adds temporary styleguide and form pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   root to: 'home#index'
 
   get '/grid', to: 'home#grid'
+  get '/forms', to: 'home#forms'
   get '/styleguide', to: 'home#styleguide'
 
   resources :activities
   resources :contacts
   resources :deadlines
+  resources :home
   resources :projects
   resources :users
 
