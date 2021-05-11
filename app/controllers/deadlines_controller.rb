fb6512a8500af64998364a4514ad0a11ff78cba4
Moritz Stuker <968496+moritzstuker@users.noreply.github.com>
2021-05-11 18:00:11 +0200

    Adds deadlines

--- app/controllers/deadlines_controller.rb
+++ app/controllers/deadlines_controller.rb
@@ -1,2 +1,10 @@
 class DeadlinesController < ApplicationController
+  def index
+    @deadlines = Deadline.all
+    @deadlines_dates = Deadline.distinct.pluck(:date).sort
+  end
+
+  def show
+    @deadline = Deadline.find(params[:id])
+  end
 end
