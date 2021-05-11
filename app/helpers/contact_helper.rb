5f097fe003764a77030341658fc7d6cb7bc5854c
Moritz Stuker <968496+moritzstuker@users.noreply.github.com>
2021-05-11 17:59:24 +0200

    Rewrites colors

--- app/helpers/contact_helper.rb
+++ app/helpers/contact_helper.rb
@@ -1,2 +1,10 @@
 module ContactHelper
+  def bg_color(str)
+    case str
+    when "client" then "green-bg"
+    when "adversary" then "red-bg"
+    when "employee" then "blue-bg"
+    when "other" then "yellow-bg"
+    end
+  end
 end
