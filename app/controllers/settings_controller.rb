80c5ac984cd201c80d366df025da2d5cadd8459c
Moritz Stuker <968496+moritzstuker@users.noreply.github.com>
2021-05-12 18:01:03 +0200

    Adds settings variables

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,2 +1,8 @@
 class SettingsController < ApplicationController
+  def index
+    @case_categories = ProjectCategory.all
+    @contact_categories = ProjectCategory.all
+    @users = User.all
+    @cases = Project.all
+  end
 end
