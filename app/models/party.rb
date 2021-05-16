56c7f15e747f1fecf156f3152fb8f5d719fdad63
Moritz Stuker <968496+moritzstuker@users.noreply.github.com>
2021-05-16 04:29:48 +0200

    Adds default value

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,4 +1,12 @@
 class Party < ApplicationRecord
   belongs_to :contact
   belongs_to :project
+
+  after_initialize  :default_values!
+
+  private
+
+  def default_values!
+    self.main = false
+  end
 end
