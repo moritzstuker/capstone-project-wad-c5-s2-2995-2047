c32909b908751347e685d57d40771bf4d260c8c1
Moritz Stuker <968496+moritzstuker@users.noreply.github.com>
2021-05-18 10:37:16 +0200

    Adds default colors to roles

--- app/models/contact_role.rb
+++ app/models/contact_role.rb
@@ -1,3 +1,10 @@
 class ContactRole < ApplicationRecord
+  DEFAULTS = {
+    'client':    '#347d39', # green
+    'adversary': '#ad2e2c', # red
+    'other':     '#daaa3f', # yellow
+    'employee':  '#4184e4', # blue
+  }
+
   has_many :contacts
 end
