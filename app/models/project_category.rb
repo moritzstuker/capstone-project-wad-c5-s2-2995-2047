e797d56f4e241e2ffaad2abc1cd65bad354f7f41
Moritz Stuker <968496+moritzstuker@users.noreply.github.com>
2021-05-18 10:37:26 +0200

    Adds default colors to categories

--- app/models/project_category.rb
+++ app/models/project_category.rb
@@ -1,3 +1,14 @@
 class ProjectCategory < ApplicationRecord
+  DEFAULTS = {
+    'bankruptcy law':        '#22272e', # dark grey
+    'civil law':             '#4184e4', # blue
+    'corporate law':         '#143d79', # dark blue
+    'criminal law':          '#922323', # red
+    'data protection':       '#545d68', # light grey
+    'immigration law':       '#347d39', # green
+    'intellectual property': '#ae7c14', # yellow
+    'labor law':             '#ae5622'  # orange
+  }
+
   has_many :projects
 end
