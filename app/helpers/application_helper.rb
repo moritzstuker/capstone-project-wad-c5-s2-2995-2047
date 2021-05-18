ede106d22c1276ea188029e773e3a783c92e82a2
Moritz Stuker <968496+moritzstuker@users.noreply.github.com>
2021-05-18 10:35:40 +0200

    Moves icon helper to more approriate location

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,11 +6,4 @@ module ApplicationHelper
   def current_user
     @current_user ||= User.find_by_id(session[:user_id]) if logged_in?
   end
-
-  def icon_tag(str)
-    doc = File.open("app/assets/images/octicons/#{str}.svg", 'r') { |f| Nokogiri::XML(f) }
-    icon = doc.at_css('svg')
-    icon['class'] = "icon #{str}"
-    raw icon
-  end
 end
--- app/helpers/layouts/application_layout_helper.rb
+++ app/helpers/layouts/application_layout_helper.rb
@@ -1,7 +1,14 @@
 module Layouts
   module ApplicationLayoutHelper
     def page_items(key)
-      eval(key.to_s.classify).all
+      eval(key.to_s.classify)
+    end
+
+    def icon_tag(str)
+      doc = File.open("app/assets/images/octicons/#{str}.svg", 'r') { |f| Nokogiri::XML(f) }
+      icon = doc.at_css('svg')
+      icon['class'] = "icon #{str}"
+      raw icon
     end
   end
 end
