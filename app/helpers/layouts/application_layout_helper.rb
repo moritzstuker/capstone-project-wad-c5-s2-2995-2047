4b3095fd122ce375c4d9d74103de09f2071b3a48
Moritz Stuker <968496+moritzstuker@users.noreply.github.com>
2021-05-27 06:54:57 +0200

    Adds custom classes to icon tag method

--- app/helpers/layouts/application_layout_helper.rb
+++ app/helpers/layouts/application_layout_helper.rb
@@ -4,10 +4,11 @@ module Layouts
       eval("#{class_name}.search(params[:q])").count
     end
 
-    def icon_tag(str)
+    def icon_tag(str, hash = {})
       doc = File.open("app/assets/images/octicons/#{str}.svg", 'r') { |f| Nokogiri::XML(f) }
       icon = doc.at_css('svg')
-      icon['class'] = "icon #{str}"
+      icon['class']  = "icon #{str}"
+      icon['class'] += " #{hash[:class]}" if hash.present?
       raw icon
     end
   end
