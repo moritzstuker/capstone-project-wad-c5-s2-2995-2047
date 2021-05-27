49a600172e209c39c65a1dc4a8cd98f4fcba1dcf
Moritz Stuker <968496+moritzstuker@users.noreply.github.com>
2021-05-27 06:58:55 +0200

    Switches fee key with default_fee

--- app/models/user_role.rb
+++ app/models/user_role.rb
@@ -1,19 +1,19 @@
 class UserRole < ApplicationRecord
   DEFAULTS = {
     'admin': {
-      'fee': 0,
+      'default_fee': 0,
       'access_level': 0
     },
     'partner': {
-      'fee': 400,
+      'default_fee': 400,
       'access_level': 1
     },
     'associate': {
-      'fee': 350,
+      'default_fee': 350,
       'access_level': 2
     },
     'intern': {
-      'fee': 180,
+      'default_fee': 180,
       'access_level': 3
     }
   }.freeze
