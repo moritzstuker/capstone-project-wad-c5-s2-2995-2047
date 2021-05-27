8b6210e548354253cb2b8f930ac0bfbdb1eb964a
Moritz Stuker <968496+moritzstuker@users.noreply.github.com>
2021-05-27 06:58:11 +0200

    Adds a belongs_to assignee and search scope

--- app/models/deadline.rb
+++ app/models/deadline.rb
@@ -3,4 +3,9 @@ class Deadline < ApplicationRecord
   CATEGORIES = ["internal", "external", "court-ordered", "legal"].freeze
 
   belongs_to :project
+  belongs_to :assignee, class_name: "User", foreign_key: "assignee_id"
+
+  scope :label_contains, -> (str) { where('label LIKE ?', "%#{str}%") }
+  scope :search,              -> (str) { label_contains(str) }
+
 end
