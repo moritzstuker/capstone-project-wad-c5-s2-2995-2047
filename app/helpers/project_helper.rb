52dd08d4e4f2bb5699424d2ce17e99da777afd4d
Moritz Stuker <968496+moritzstuker@users.noreply.github.com>
2021-05-27 06:55:08 +0200

    Adds new method for urgency colors

--- app/helpers/project_helper.rb
+++ app/helpers/project_helper.rb
@@ -2,4 +2,14 @@ module ProjectHelper
   def fee (int)
     "#{number_to_currency(int, unit: "fr.", separator: ".", delimiter: "'", format: "%n %u")}".html_safe
   end
+
+  def urgency_by_date(date)
+    if date.to_date == Date.today
+      ' red-bg'
+    elsif date.to_date == Date.tomorrow
+      ' orange-bg'
+    elsif date.to_date > Date.today
+      ' green-bg'
+    end
+  end
 end
