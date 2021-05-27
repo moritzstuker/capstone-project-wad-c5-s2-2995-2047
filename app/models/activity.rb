2ee0bee6ee16c81c24e0326cd2a3d0bade5db711
Moritz Stuker <968496+moritzstuker@users.noreply.github.com>
2021-05-27 06:55:20 +0200

    Minor tweaks to model

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,7 +1,14 @@
 class Activity < ApplicationRecord
 
-  CATEGORIES = ["Email/Letter", "Meeting", "Phone call", "Court hearing"].freeze
+  CATEGORIES = [
+    "Email/Letter",
+    "Meeting",
+    "Phone call",
+    "Court hearing"
+  ].freeze
 
   belongs_to :project
   belongs_to :user
+
+  before_validation { self.fee ||= 0 }
 end
