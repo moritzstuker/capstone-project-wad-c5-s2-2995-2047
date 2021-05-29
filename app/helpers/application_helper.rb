8ecd9183ea7e6b2c67bd1046e52b3362ef3f816d
Moritz Stuker <968496+moritzstuker@users.noreply.github.com>
2021-05-29 22:10:34 +0200

    Adds sidebar label method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
   def current_user
     @current_user ||= User.find_by_id(session[:user_id]) if logged_in?
   end
+
+  def sidebar_text (str, sym)
+    raw "<span>#{ str }</span><span class=\"counter\">#{ page_items(sym.to_s.classify) }</span>"
+  end
 end
