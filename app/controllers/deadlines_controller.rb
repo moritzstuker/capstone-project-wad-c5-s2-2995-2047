11abb2ccf7db942e41834ee76686aac0e2b34c7b
Moritz Stuker <968496+moritzstuker@users.noreply.github.com>
2021-06-06 22:11:37 +0200

    Adds filtering

--- app/controllers/deadlines_controller.rb
+++ app/controllers/deadlines_controller.rb
@@ -1,6 +1,10 @@
 class DeadlinesController < ApplicationController
+  before_action -> { restrict_access(3) }
+
   def index
-    @deadlines = Deadline.filter(params.slice(:assignee)).search(params[:q]).order('date ASC')
+    @deadlines = Deadline.filter(params.slice(:user, :category, :show, :urgency)).order(date: :asc) # filters
+    @deadlines = @deadlines.where(completed_at: nil) unless params[:show].present?
+    @deadlines = @deadlines.search(params[:q]) if params[:q].present? # searches
   end
 
   def show
