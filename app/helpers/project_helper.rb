065016509cbd6a7ac33e4298c295982f4158dc02
Moritz Stuker <968496+moritzstuker@users.noreply.github.com>
2021-06-06 22:10:26 +0200

    Removes method which is now part of the model

--- app/helpers/project_helper.rb
+++ app/helpers/project_helper.rb
@@ -2,14 +2,4 @@ module ProjectHelper
   def fee (int)
     "#{number_to_currency(int, unit: "fr.", separator: ".", delimiter: "'", format: "%n %u")}".html_safe
   end
-
-  def urgency_by_date(date, bool = false)
-    if date.to_date == Date.today
-      ' red'
-    elsif date.to_date == Date.tomorrow
-      ' orange'
-    elsif date.to_date > Date.tomorrow && bool
-      ' green'
-    end
-  end
 end
