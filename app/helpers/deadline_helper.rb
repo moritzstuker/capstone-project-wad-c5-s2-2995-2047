55261c8742df8fa031e99fc862f999bb8cfc3db5
Moritz Stuker <968496+moritzstuker@users.noreply.github.com>
2021-06-06 22:10:43 +0200

    Adds method for coloring based on urgency (renders class)

--- app/helpers/deadline_helper.rb
+++ app/helpers/deadline_helper.rb
@@ -1,2 +1,11 @@
 module DeadlineHelper
+  def urgency_by_date(date)
+    if Deadline.dates_by_urgency('0').include?(date.to_date)
+      ' red'
+    elsif Deadline.dates_by_urgency('1').include?(date.to_date)
+      ' orange'
+    else
+      ' green'
+    end
+  end
 end
