b92c9e89b7c6e51950b81597d04f5e284a6255c2
Moritz Stuker <968496+moritzstuker@users.noreply.github.com>
2021-06-06 22:08:10 +0200

    Adds coloring based on urgency

--- app/models/deadline.rb
+++ app/models/deadline.rb
@@ -4,10 +4,30 @@ class Deadline < ApplicationRecord
   CATEGORIES = ["internal", "external", "court-ordered", "legal"].freeze
 
   belongs_to :project
-  belongs_to :assignee, class_name: "User", foreign_key: "assignee_id"
+  belongs_to :assignee, class_name: "User"
 
-  scope :label_contains, -> (str) { where('label LIKE ?', "%#{str}%") }
-  scope :search,              -> (str) { label_contains(str) }
+  scope :search, -> (str) { where('label LIKE ?', "%#{str}%") }
 
-  scope :filter_by_assignee, -> (str) { where( assignee_id: str ) }
+  scope :filter_by_category, -> (str) { where(category: str) }
+  scope :filter_by_show,   -> (str) {
+    if str == 'active'
+      where(completed_at: nil)
+    elsif str == 'inactive'
+      where.not(completed_at: nil)
+    end
+  }
+  scope :filter_by_urgency,  -> (str) { where(date: dates_by_urgency(str)) }
+  scope :filter_by_user,     -> (str) { where(assignee: str) }
+  
+  private
+
+  def self.dates_by_urgency(str)
+    dates = Deadline.where(completed_at: nil).distinct.pluck(:date).sort
+    case
+    when str == '0' && dates.count >= 2 then dates[0..1]
+    when str == '1' && dates.count >= 4 then dates[2..3]
+    when str == '2' && dates.count >= 5 then dates[4..-1]
+    else dates
+    end
+  end
 end
