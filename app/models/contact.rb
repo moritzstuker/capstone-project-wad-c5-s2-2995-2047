00b6ec277d1aaad6cbe5184889e927327cfee476
Moritz Stuker <968496+moritzstuker@users.noreply.github.com>
2021-06-08 13:28:37 +0200

    Removes (too) risky eval in model and replaces it with safer method

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,27 +1,9 @@
 class Contact < ApplicationRecord
   include Filtering
 
-  CATEGORIES = [
-    'natural person',
-    'legal person'
-  ].freeze
-
-  FORMATS = {
-    name:       '#{first_name} #{last_name} #{suffix}',
-    short_name: '#{first_name.to_s.gsub(/(([[:alpha:]])[[:alpha:]]*\.?)/, \'\2.\')} #{last_name}',
-    long_name:  '#{prefix} #{first_name} #{last_name} #{suffix}',
-    full_name:  '#{prefix} #{first_name} #{last_name} #{suffix}<span class=\"mute\"> (#{combine(:category)})</span>',
-    initials:   '#{first_name.to_s.gsub(/(([[:alpha:]])[[:alpha:]]*\.?)/, \'\2.\')} #{last_name.to_s.chars.first}.',
-    address:    '#{address[:pobox]}<br>#{address[:street]}&nbsp;#{address[:streetno]}<br>#{address[:zip]}&nbsp;#{address[:city]}<br>#{address[:country]}',
-    city:       '#{address[:zip]} #{address[:city]} (#{address[:country]})',
-    street:     '#{address[:street]} #{address[:streetno]}',
-    category:   '#{category == \'natural person\' ? \'Private person\' : \'Company\'}',
-    role:       '#{role.titleize}'
-  }
-
-  belongs_to :role, class_name: "ContactRole", foreign_key: "contact_role_id"
-  has_one    :user
+  belongs_to              :role, class_name: "ContactRole", foreign_key: "contact_role_id"
   belongs_to              :address, class_name: "ContactAddress", foreign_key: "contact_address_id"
+  has_one                 :user
   has_and_belongs_to_many :projects
 
   scope :search,              -> (str) { where('first_name LIKE ? OR last_name LIKE ? OR suffix LIKE ?', "%#{str}%", "%#{str}%", "%#{str}%") }
@@ -33,22 +15,27 @@ class Contact < ApplicationRecord
   scope :other,       ->       { get_role('other') }
 
   scope :filter_by_role, -> (str) { where(role: str) }
-  scope :filter_by_category, -> (str) { where(category: str) }
+  scope :filter_by_category, -> (str) { where(company: str) }
 
-  after_initialize  :default_values!
-  before_validation { self.email = email.downcase }
-
-  validates :first_name, length: { maximum: 60 }
-  validates :last_name,  presence: true, length: { maximum: 60 }
+  validates :first_name, length: { maximum: 15 }
+  validates :last_name,  presence: true, length: { maximum: 30 }
   validates :email,      presence: true, length: { minimum: 6, maximum: 60 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: true
 
   def combine(format = :name)
-    eval('"' + FORMATS[format.to_sym] + '"').gsub(/^\s*(?:<br\s*\/?\s*>)+|(?:<br\s*\/?\s*>)+\s*$/i, "").gsub(/\s+/, " ").strip.html_safe
-  end
 
-  private
+    formats = {
+      name:      [first_name, last_name, " "],
+      full_name: [prefix, first_name, last_name, suffix, " "],
+      address:   [address.pobox, "#{address.street} #{address.streetno}", "#{address.zip} #{address.city}", address.country, "\n"],
+      category:  "#{company ? 'Company' : 'Private person'}"
+    }
 
-  def default_values!
-    # nothing for now...
+    if formats[format.to_sym].is_a?(Array)
+      separator = formats[format.to_sym].pop
+      formats[format.to_sym].reject(&:blank?).join(separator)
+
+    else
+      formats[format.to_sym]
+    end
   end
 end
