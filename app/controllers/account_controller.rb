5700dbf9aa14ad7c1f8dc52022cce075c8b107d9
Moritz Stuker <968496+moritzstuker@users.noreply.github.com>
2021-06-09 22:42:16 +0200

    Fixes account page

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -5,7 +5,31 @@ class AccountController < ApplicationController
   end
 
   def update
-    current_user.update(user_params)
-    redirect_to account_path
+    respond_to do |format|
+      if current_user.update(user_params)
+        format.html { redirect_to account_path, notice: 'Account updated' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user)
+    .permit(
+      :login,
+      :password,
+      :avatar,
+      :preferred_lang,
+      contact_attributes: [
+        :id,
+        :first_name,
+        :last_name,
+        :email,
+        :birthday
+      ]
+    )
   end
 end
