024943e6c6c737e9b671dd90a967c32ac82d63c5
Moritz Stuker <968496+moritzstuker@users.noreply.github.com>
2021-06-11 13:08:25 +0200

    Increases security replacing html_safe by sanitize

--- app/helpers/project_helper.rb
+++ app/helpers/project_helper.rb
@@ -1,6 +1,6 @@
 module ProjectHelper
   def fee (int)
-    "#{number_to_currency(int, unit: "fr.", separator: ".", delimiter: "'", format: "%n %u")}".html_safe
+    number_to_currency(int, unit: "fr.", separator: ".", delimiter: "'", format: "%n %u")
   end
 
   def main_parties (obj, bool = true)
@@ -8,20 +8,19 @@ module ProjectHelper
     str  = ""
 
     if bool
-      str += "#{obj.clients.first.combine(:name)}" if obj.clients.exists?
+      str += "#{build_label(obj.clients.first, :name)}" if obj.clients.exists?
       str += "&nbsp;et al." if obj.clients.count > 1
-      str += "&nbsp;V.&nbsp;" if obj.clients.exists? && obj.adversaries.exists?
-      str += "#{obj.adversaries.first.combine(:name)}" if obj.adversaries.exists?
+      str += "&nbsp;v.&nbsp;" if obj.clients.exists? && obj.adversaries.exists?
+      str += "#{build_label(obj.adversaries.first, :name)}" if obj.adversaries.exists?
       str += "&nbsp;et al." if obj.adversaries.count > 1
-      str  = "<span class=\"mute\">#{str}</span>"
     else
-      str += link_to(obj.clients.first.combine(:name), obj.clients.first) if obj.clients.exists?
+      str += link_to(build_label(obj.clients.first, :name), obj.clients.first) if obj.clients.exists?
       str += "&nbsp;<span class=\"mute\">et al.</span>" if obj.clients.count > 1
       str += "<span class=\"mute\">&nbsp;v.&nbsp;</span>" if obj.clients.exists? && obj.adversaries.exists?
-      str += link_to(obj.adversaries.first.combine(:name), obj.adversaries.first) if obj.adversaries.exists?
+      str += link_to(build_label(obj.adversaries.first, :name), obj.adversaries.first) if obj.adversaries.exists?
       str += "&nbsp;<span class=\"mute\">et al.</span>" if obj.adversaries.count > 1
     end
 
-    str.html_safe
+    sanitize str
   end
 end
