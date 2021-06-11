dc425fb195ef01536202d46035d595d5ac58fc10
Moritz Stuker <968496+moritzstuker@users.noreply.github.com>
2021-06-11 19:40:34 +0200

    Adds new contact page

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,7 @@
 class ContactsController < ApplicationController
-  before_action :set_contact,  only: %i[ show ]
+  before_action :set_contact, only: %i[ show ]
+  before_action :can_edit?,   only: %i[ edit update ]
+  before_action :can_delete?, only: %i[ destroy ]
   helper_method :can_edit?, :can_delete?
 
   def index
@@ -14,10 +16,50 @@ class ContactsController < ApplicationController
   def show
   end
 
+  def new
+    @contact = Contact.new
+  end
+
+  def edit
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+
+    respond_to do |format|
+      if @contact.save
+        format.html { redirect_to @contact, notice: "Contact was successfully created." }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @contact.update(contact_params)
+        format.html { redirect_to @contact, notice: "Contact was successfully updated." }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @contact.destroy
+    respond_to do |format|
+      format.html { redirect_to contacts_url, notice: "Contact was successfully destroyed." }
+    end
+  end
+
   private
 
   def set_contact
     @contact = Contact.find(params[:id])
+  end
+
+  def contact_params
+    params.require(:contact).permit()
   end
 
   def can_edit?(contact = @contact, user = current_user)
