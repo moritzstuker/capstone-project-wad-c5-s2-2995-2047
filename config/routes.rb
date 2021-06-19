531032ef4a8c8acb7f3d233ecd88a9c2a597947a
Moritz Stuker <968496+moritzstuker@users.noreply.github.com>
2021-06-19 18:55:43 +0200

    Nests activities under projects

--- config/routes.rb
+++ config/routes.rb
@@ -10,18 +10,21 @@ Rails.application.routes.draw do
     post   '/login',  to: 'sessions#create'
     delete '/logout', to: 'sessions#destroy'
 
-    get  '/signup', to: 'users#new'
+    get    '/signup', to: 'users#new'
 
-    get   '/dashboard', to: 'dashboard#index'
+    get    '/dashboard', to: 'dashboard#index'
 
-    get   '/account', to: 'account#edit'
-    patch '/account', to: 'account#update'
+    get    '/account', to: 'account#edit'
+    patch  '/account', to: 'account#update'
 
     resources :contacts
     resources :deadlines
-    resources :projects
     resources :project_categories
     resources :sessions, only: [:new, :create, :destroy]
     resources :users
+
+    resources :projects do
+      resources :activities
+    end
   end
 end
