b01f69fe1e15547b931586e8b2c5040fd81911a3
Moritz Stuker <968496+moritzstuker@users.noreply.github.com>
2021-06-21 08:08:37 +0200

    Removes build_name method and replaces it with call to .name field

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -4,19 +4,19 @@ module ProjectsHelper
   end
 
   def main_parties (project)
-    clients = link_to(build_name(project.clients.first), project.clients.first) if project.clients.exists?
+    clients = link_to(project.clients.first.name, project.clients.first) if project.clients.exists?
     clients_suffix = "<span class=\"mute\">et al.</span>" if project.clients.count > 1
     versus = "<span class=\"mute\">v.</span>" if project.clients.exists? && project.adversaries.exists?
-    adversaries = link_to(build_name(project.adversaries.first), project.adversaries.first) if project.adversaries.exists?
+    adversaries = link_to(project.adversaries.first.name, project.adversaries.first) if project.adversaries.exists?
     adversaries_suffix = "<span class=\"mute\">et al.</span>" if project.adversaries.count > 1
     sanitize([clients, clients_suffix, versus, adversaries, adversaries_suffix].reject(&:blank?).join(' '))
   end
 
   # def main_parties (project)
-  #   clients = link_to(build_name(project.clients.first), project.clients.first) if project.clients.exists?
+  #   clients = link_to(project.clients.first.name, project.clients.first) if project.clients.exists?
   #   clients_suffix = "<span class=\"mute\">et al.</span>" if project.clients.count > 1
   #   versus = "<span class=\"mute\">v.</span>" if project.clients.exists? && project.adversaries.exists?
-  #   adversaries = link_to(build_name(project.adversaries.first), project.adversaries.first) if project.adversaries.exists?
+  #   adversaries = link_to(project.adversaries.first.name, project.adversaries.first) if project.adversaries.exists?
   #   adversaries_suffix = "<span class=\"mute\">et al.</span>" if project.adversaries.count > 1
   #   sanitize([clients, clients_suffix, versus, adversaries, adversaries_suffix].reject(&:blank?).join(' '))
   # end
