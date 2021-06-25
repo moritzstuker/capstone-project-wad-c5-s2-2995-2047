d107c350ec6921d521399290c2e039dde2864ae4
Moritz Stuker <968496+moritzstuker@users.noreply.github.com>
2021-06-25 08:04:16 +0200

    Deletes unused method

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -11,13 +11,4 @@ module ProjectsHelper
     adversaries_suffix = "<span class=\"mute\">et al.</span>" if project.adversaries.count > 1
     sanitize([clients, clients_suffix, versus, adversaries, adversaries_suffix].reject(&:blank?).join(' '))
   end
-
-  # def main_parties (project)
-  #   clients = link_to(project.clients.first.name, project.clients.first) if project.clients.exists?
-  #   clients_suffix = "<span class=\"mute\">et al.</span>" if project.clients.count > 1
-  #   versus = "<span class=\"mute\">v.</span>" if project.clients.exists? && project.adversaries.exists?
-  #   adversaries = link_to(project.adversaries.first.name, project.adversaries.first) if project.adversaries.exists?
-  #   adversaries_suffix = "<span class=\"mute\">et al.</span>" if project.adversaries.count > 1
-  #   sanitize([clients, clients_suffix, versus, adversaries, adversaries_suffix].reject(&:blank?).join(' '))
-  # end
 end
