891a8b63ceb174e543255c52fc28ca2d8cc9c60a
Moritz Stuker <968496+moritzstuker@users.noreply.github.com>
2021-07-14 18:39:08 +0000

    Adds controller tests

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -11,7 +11,7 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
 
     log_in_as(@user)
     get user_url(@user)
-    assert_response :success
+    assert_redirected_to dashboard_url
   end
 
   [
@@ -40,8 +40,39 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
     #'new_user_url',
   ].each do |url|
     test "should not have access to #{url}" do
-      get eval(url)
+      get eval(url) # Yes, eval() is extremely dangerous and may create a security hole. But here 1) there's no user input and 2) it's only run in a test env, so the risk is nil.
       assert_redirected_to login_url
+    end
+  end
+
+  [
+    #'root_url',
+    #'account_url',
+    #'contacts_url',
+    'contact_url(1)',
+    'edit_contact_url(1)',
+    #'contacts_import_url',
+    #'new_contact_url',
+    #'dashboard_url',
+    #'deadlines_url',
+    'edit_deadline_url(1)',
+    #'new_deadline_url',
+    #'login_url',
+    #'projects_url',
+    'project_url(1)',
+    'edit_project_url(1)',
+    #'new_project_url',
+    #'search_url',
+    #'new_session_url',
+    #'signup_url',
+    'users_url',
+    'user_url(1)',
+    'edit_user_url(1)',
+    #'new_user_url',
+  ].each do |url|
+    test "should not have access to restricted url #{url}" do
+      get eval(url)
+      assert_response :redirect
     end
   end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,12 +2,7 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @user = users(:one)
-  end
-
-  test "should get index" do
-    get users_url
-    assert_response :success
+    @user = users(:registered)
   end
 
   test "should get new" do
@@ -17,28 +12,35 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "should create user" do
     assert_difference('User.count') do
-      post users_url, params: { user: { avatar: @user.avatar, default_fee: @user.default_fee, email: @user.email, name: @user.name, locale: @user.locale, login: @user.login, password_digest: @user.password_digest, role: @user.role } }
+      post users_url, params: {
+        user: {
+          login: 'new@epfl.ch',
+          name: 'Jack the Fixture',
+          password: 'password',
+          password_confirmation: 'password',
+          email: 'jack@example.com'
+        }
+      }
     end
 
     assert_redirected_to user_url(User.last)
   end
 
-  test "should show user" do
+  test "should show dashboard" do
+    log_in_as(@user)
     get user_url(@user)
-    assert_response :success
+    assert_redirected_to dashboard_url
   end
 
   test "should get edit" do
+    log_in_as users(:admin)
     get edit_user_url(@user)
     assert_response :success
   end
 
-  test "should update user" do
-    patch user_url(@user), params: { user: { avatar: @user.avatar, default_fee: @user.default_fee, email: @user.email, name: @user.name, locale: @user.locale, login: @user.login, password_digest: @user.password_digest, role: @user.role } }
-    assert_redirected_to user_url(@user)
-  end
-
   test "should destroy user" do
+    log_in_as users(:admin)
+
     assert_difference('User.count', -1) do
       delete user_url(@user)
     end
