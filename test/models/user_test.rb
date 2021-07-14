603ba997fd9baf75b865edde870fdcc2eda1d06e
Moritz Stuker <968496+moritzstuker@users.noreply.github.com>
2021-07-14 19:03:40 +0000

    Removes unused tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,8 +1,34 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "create a user" do
-  #   user = User.new email: 'user@email.com', password: 'password'
-  #   assert user.save
-  # end
+  test "email must be valid" do
+    user_1 = User.new login: 'user_1', name: 'Jack the Fixture', password: 'password', password_confirmation: 'password', email: 'jackexample.com'
+    refute user_1.valid?
+    user_2 = User.new login: 'user_2', name: 'Jake the Fixture', password: 'password', password_confirmation: 'password', email: 'jake@examplecom'
+    refute user_2.valid?
+    user_3 = User.new login: 'user_3', name: 'John the Fixture', password: 'password', password_confirmation: 'password', email: 'john@'
+    refute user_3.valid?
+    user_4 = User.new login: 'user_4', name: 'Jody the Fixture', password: 'password', password_confirmation: 'password', email: 'jody@example.com'
+    assert user_4.valid?
+  end
+
+  test "login must be unique" do
+    User.create! login: 'user', name: 'Jack the Fixture', password: 'password', password_confirmation: 'password', email: 'jack@example.com'
+    user = User.new login: 'user', name: 'Jake the Fixture', password: 'password', password_confirmation: 'password', email: 'jake@example.com'
+    refute user.valid?
+  end
+
+  test "email must be unique" do
+    User.create! login: 'user_1', name: 'Jack the Fixture', password: 'password', password_confirmation: 'password', email: 'jack@example.com'
+    user = User.new login: 'user_2', name: 'Jake the Fixture', password: 'password', password_confirmation: 'password', email: 'jack@example.com'
+    refute user.valid?
+  end
+
+  test "sets correct defaults" do
+    user = User.new login: 'user', name: 'Jake the Fixture', password: 'password', password_confirmation: 'password', email: 'jack@example.com'
+    assert_equal user.default_fee, 300.00
+    assert_equal user.avatar, 'default-avatar.png'
+    assert_equal user.role, :intern
+    assert_equal user.locale, 'en'
+  end
 end
--- test/system/contact_roles_test.rb
+++
@@ -1,9 +0,0 @@
-require "application_system_test_case"
-
-class ContactRolesTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit contact_roles_url
-  #
-  #   assert_selector "h1", text: "ContactRole"
-  # end
-end
