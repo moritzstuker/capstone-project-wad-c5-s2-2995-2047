75e9aeb6ea0b5c514a033fa6f474d1647af7381b
Moritz Stuker <968496+moritzstuker@users.noreply.github.com>
2021-07-18 11:20:04 +0000

    Moves away from using distinct.pluck where unnecessary

--- app/controllers/deadlines_controller.rb
+++ app/controllers/deadlines_controller.rb
@@ -2,13 +2,14 @@ class DeadlinesController < ApplicationController
   before_action :set_deadline, only: %i[ update destroy complete ]
 
   def index
+    @all_deadlines_reset = Deadline.where(completed_at: nil)
     @all_deadlines = Deadline.filter(params.slice(:query, :category, :urgency, :user)).order(:date, :label) # filters
     @deadlines = params[:completed] == 'true' ? @all_deadlines : @all_deadlines.where(completed_at: nil)
 
     @deadlines_by_dates = @deadlines.group_by(&:date).sort
     @deadlines_by_dates = Kaminari.paginate_array(@deadlines_by_dates).page(params[:page]).per(10)
 
-    @categories = @deadlines.distinct.pluck(:category).sort
+    @categories = Deadline::CATEGORIES.sort
     @users = User.all
 
     flash.now[:notice] = "#{ t('.no_results') }." if params[:query].present? && @deadlines.count == 0
