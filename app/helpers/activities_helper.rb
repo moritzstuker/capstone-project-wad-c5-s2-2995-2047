ef3f1ddf8f076788223ceab107f93f1533bdab22
Moritz Stuker <968496+moritzstuker@users.noreply.github.com>
2021-08-30 19:14:00 +0200

    Fixes incorrect activity total fee calculation

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -2,4 +2,12 @@ module ActivitiesHelper
   def can_delete_activity?(activity, user)
     user == activity.project.owner || user == activity.user || can_delete?(activity.project, user)
   end
+
+  def total_billed(activities)
+    total = 0
+    activities.each do |activity|
+      total += activity.fee * activity.duration
+    end
+    return total
+  end
 end
